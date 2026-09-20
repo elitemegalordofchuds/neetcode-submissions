6e16c6b065cc846a907f73e13b430c62bd9d3a13
elitemegalordofchuds <omarmohddnaji@gmail.com>
2026-09-21 02:21:40 +0300

    Add: minimum-window-with-characters - submission-33

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-33.cpp
@@ -0,0 +1,58 @@
+class Solution {
+public:
+    string minWindow(string s, string t) {
+        if (t.length() > s.length()) return{};
+        int right{};
+        int left{};
+        int minlength{};
+        int required{};
+        int formed{};
+        int minleft{};
+        std::vector<int> target(128);
+        std::vector<int> window(128);
+        for (char c : t) target[c]++;
+        for (int count : target) if (count > 0) required++;
+        
+        while (right < s.length()){
+            char c = s[right];
+            window[c]++;
+            if (window[c] == target[c]) formed++;
+            if (formed == required) break;
+            right++;
+        }
+        if (formed != required) return{};
+
+        while (left <= right){
+            char c = s[left];
+            if (window[c] == target[c]) break;
+            window[c]--;
+            left++;
+        }
+
+        minlength = right - left + 1;
+        minleft = left;
+
+        while (right < s.length() - 1){
+            char c_out{s[left]};       
+            if (window[c_out] == target[c_out]) formed--;
+            window[c_out]--;   
+            left++;
+
+            right++;
+            window[s[right]]++;
+            if (window[s[right]] == target[s[right]]) formed++;
+            
+            while (formed == required){  
+                minlength = right - left + 1;
+                minleft = left;
+                char cl = s[left];
+                window[cl]--;          
+                if (window[cl] < target[cl]){
+                    formed--; 
+                }
+                left++;
+            }
+        }
+        return s.substr(minleft, minlength);
+    }
+};
